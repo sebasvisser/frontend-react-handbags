7e5a5623a1d99ca286b05e33df86ab1f9bb3cbf9
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-09 17:15:44 +0200

    articles toegevoegd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React
 import './App.css';
 import Buttonclick
     from "./components/Buttonclick";
+import bag1
+    from "./assets/bag_1.png";
+import bag2
+    from "./assets/bag_2.png";
+import bag3
+    from "./assets/bag_3.png";
+import bag4
+    from "./assets/bag_4.png";
 
 function App() {
   return (
@@ -13,6 +21,32 @@ function App() {
               <Buttonclick>shop all bags</Buttonclick>
               <Buttonclick disabled={true}>pre-orders</Buttonclick>
           </nav>
+          <main>
+              <article>
+                  <span>Best seller</span>
+                  <img alt="The handy bag" src={bag1} />
+                  <p>The handy bag</p>
+                  <h4>400,-</h4>
+              </article>
+              <article>
+                  <span>Best seller</span>
+                  <img alt="The stylish bag" src={bag2}/>
+                  <p>The stylish bag</p>
+                  <h4>250,-</h4>
+              </article>
+              <article>
+                  <span>New collection</span>
+                  <img alt="The simple bag" src={bag3}/>
+                  <p>The simple bag</p>
+                  <h4>300,-</h4>
+              </article>
+              <article>
+                  <span>New collection</span>
+                  <img alt="The trendy bag" src={bag4}/>
+                  <p>The trendy bag</p>
+                  <h4>150,-</h4>
+              </article>
+          </main>
       </>
   );
 }
