1b8446d85a9242d8e32b570dc0d419bb19578623
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-09 15:55:58 +0200

    3 buttons gemaakt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-      <h1>Handbags & Purses</h1>
+      <>
+          <h1>Handbags & Purses</h1>
+          <nav>
+            <button>to the collection</button>
+            <button>shop all bags</button>
+            <button>pre-orders</button>
+          </nav>
+      </>
   );
 }
 
